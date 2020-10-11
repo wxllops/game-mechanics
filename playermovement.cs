ca6c9d275c36943fa51c5c046b6e213753d7f187
wxllops <40343857+wxllops@users.noreply.github.com>
2020-10-11 11:16:18 +1100

    Add files via upload
    Unity API.

---
+++ playermovement.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+    // Reference for Rigidbody
+public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float SidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);  
+        // moving left and right
+        if (Input.GetKey("d") )
+        {
+            rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
